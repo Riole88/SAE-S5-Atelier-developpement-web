359ffe7a614ae96c08382128ac95dd5a0d23aa2c
tuline-leveque <tuline.leveque@gmail.com>
2025-10-21 10:19:05 +0200

    update(service): Outil
    Ajout du serviceOutil avec les fonctions getOutils (avoir la liste des outils) et getOutil($id) (avoir un outil).

---
+++ app/src/application_core/application/usecases/ServiceOutil.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace charlymatloc\core\application\usecases;
+
+use charlymatloc\api\dto\OutilDTO;
+use charlymatloc\core\application\usecases\interface\ServiceOutilInterface;
+use charlymatloc\core\domain\entities\Outil\Outil;
+use charlymatloc\infra\repositories\interface\OutilRepositoryInterface;
+
+class ServiceOutil implements ServiceOutilInterface {
+    private OutilRepositoryInterface $outilRepository;
+
+    public function __construct(OutilRepositoryInterface $outilRepository)
+    {
+        $this->outilRepository = $outilRepository;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function getOutils(): array {
+//        try {
+        $outils = $this->outilRepository->findAllOutils();
+//        } catch (\Exception $e) {
+//            throw new \Exception($e->getMessage());
+//        }
+
+        $res = [];
+        foreach ($outils as $outil) {
+            $res[] = new OutilDTO(
+                $outil->id,
+                $outil->nom,
+                $outil->description,
+                $outil->image,
+                $outil->tarifJournalier,
+                $outil->quantiteStock,
+                $outil->idCat,
+                $outil->creePar,
+                $outil->creeQuand,
+                $outil->modifiePar,
+                $outil->modifieQuand
+            );
+        }
+        return $res;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function getOutil(string $id): Outil
+    {
+//        try {
+            $outil = $this->outilRepository->findOutilById($id);
+//        } catch (EntityNotFoundException $e) {
+//            throw new EntityNotFoundException($e->getEntity()." not found", $e->getEntity());
+//        } catch (\Exception $e) {
+//            throw new \Exception("probleme lors de la reception du patient.", $e->getCode());
+//        }
+
+        return $outil;
+    }
+}
