b5cfa1e949e5e2bc07f4bbcff0bbfbb0f61653bd
Riole88 <boullemanitou@gmail.com>
2025-10-21 09:37:12 +0200

    ajout Reservation.php

--- app/src/application_core/domain/entities/Utilisateur/Reservation.php
+++ app/src/application_core/domain/entities/Utilisateur/Reservation.php
@@ -2,7 +2,26 @@
 
 namespace charlymatloc\core\domain\entities\Utilisateur;
 
+use Faker\Core\Uuid;
+
 class Reservation
 {
+    public function __construct(
+        private Uuid $id,
+        private Uuid $idUser,
+        private \DateTimeInterface $dateDebut,
+        private \DateTimeInterface $dateFin,
+        private string $statut,
+        private ?Uuid $creePar,
+        private \DateTimeInterface $creeQuand,
+        private ?Uuid $modifiePar,
+        private \DateTimeInterface $modifieQuand
+    ){}
 
+    public function __get(string $name){
+        if(property_exists($this,$name)) {
+            return $this->$name;
+        }
+        throw new \Exception("Propriété '$name' inexistante dans " . __CLASS__);
+    }
 }
