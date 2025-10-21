a64d5b834e51b3840a32fa17716eeb292d901b30
Riole88 <boullemanitou@gmail.com>
2025-10-21 09:09:56 +0200

    ajout PanierOutil.php

--- app/src/application_core/domain/entities/PanierDetail/PanierOutil.php
+++ app/src/application_core/domain/entities/PanierDetail/PanierOutil.php
@@ -2,7 +2,26 @@
 
 namespace charlymatloc\core\domain\entities\PanierDetail;
 
+use Faker\Core\Uuid;
+
 class PanierOutil
 {
+    public function __construct(
+        private Uuid $id,
+        private Uuid $idPanier,
+        private Uuid $idOutil,
+        private int $quantite,
+        private \DateTimeImmutable $dateReservation,
+        private ?Uuid $creePar,
+        private \DateTimeImmutable $creeQuand,
+        private ?Uuid $modifiePar,
+        private \DateTimeImmutable $modifieQuand
+    ){}
 
+    public function __get(string $name){
+        if(property_exists($this,$name)) {
+            return $this->$name;
+        }
+        throw new Exception("Propriété '$name' inexistante dans " . __CLASS__);
+    }
 }
