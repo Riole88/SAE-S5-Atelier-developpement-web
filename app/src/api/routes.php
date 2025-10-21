5aaab3f8c5c6d858d75e7bc728846ce5c6869d54
Riole88 <boullemanitou@gmail.com>
2025-10-21 16:20:48 +0200

    add(middleware) CorsMiddleware.php update(routeMiddleware) route.php
    php

--- app/src/api/routes.php
+++ app/src/api/routes.php
@@ -1,11 +1,13 @@
 <?php
 declare(strict_types=1);
 
+use charlymatloc\api\middlewares\CorsMiddleware;
 use Slim\App;
 use charlymatloc\api\actions\OutilAction;
 use charlymatloc\api\actions\OutilsAction;
 
 return function(App $app): App {
+    $app->add(CorsMiddleware::class);
 
     //    GET
     $app->get('/outils', OutilsAction::class);
