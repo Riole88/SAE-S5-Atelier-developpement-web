b5a7a53978bf1f256517c3b0c089df812d3bb02a
Riole88 <boullemanitou@gmail.com>
2025-10-21 08:46:25 +0200

    Ajout entite outil

--- app/src/application_core/application/ports/spi/repositoryInterfaces/OutilRepositoryInterface.php
+++ app/src/application_core/application/ports/spi/repositoryInterfaces/OutilRepositoryInterface.php
@@ -2,7 +2,7 @@
 
 namespace charlymatloc\core\application\ports\spi\repositoryInterfaces;
 
-use charlymatloc\core\domain\entities\outil\Outil;
+use charlymatloc\core\domain\entities\Outil\Outil;
 
 interface OutilRepositoryInterface {
     public function findById(string $id): Outil;
---
+++ app/src/application_core/domain/entities/Outil/Outil.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace charlymatloc\core\domain\entities\Outil;
+
+use Faker\Core\Uuid;
+
+class Outil {
+    public function __construct(
+        private UUID $id,
+        private string $nom,
+        private string $desc,
+        private string $image,
+        private float $tarifJournalier,
+        private int $quantite,
+        private UUID $idCat,
+        private UUID $creePar,
+        private \DateTimeImmutable $creeQuand,
+        private UUID $modifiePar,
+        private \DateTimeImmutable $modifieQuand)
+    {}
+
+    public function getId(): Uuid
+    {
+        return $this->id;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function getDesc(): string
+    {
+        return $this->desc;
+    }
+
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    public function getTarifJournalier(): float
+    {
+        return $this->tarifJournalier;
+    }
+
+    public function getQuantite(): int
+    {
+        return $this->quantite;
+    }
+
+    public function getIdCat(): Uuid
+    {
+        return $this->idCat;
+    }
+
+    public function getCreePar(): Uuid
+    {
+        return $this->creePar;
+    }
+
+    public function getCreeQuand(): \DateTimeImmutable
+    {
+        return $this->creeQuand;
+    }
+
+    public function getModifiePar(): Uuid
+    {
+        return $this->modifiePar;
+    }
+
+    public function getModifieQuand(): \DateTimeImmutable
+    {
+        return $this->modifieQuand;
+    }
+}
