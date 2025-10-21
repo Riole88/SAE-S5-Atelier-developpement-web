69f962aaa1b8dc5d5094cfd4f9c58b77c99d492f
Kinnig Felicien <felicien.kinnig9@etu.univ-lorraine.fr>
2025-10-21 10:46:59 +0200

    Update(repository): PDOOutilRepository
    changement du nom de constructeur du repository

--- app/src/infrastructure/repositories/PDOOutilRepository.php
+++ app/src/infrastructure/repositories/PDOOutilRepository.php
@@ -3,10 +3,6 @@
 namespace charlymatloc\infra\repositories;
 
 use charlymatloc\core\domain\entities\outil\Outil;
-//use charlymatloc\core\domain\entities\Panier\Panier;
-//use charlymatloc\core\domain\entities\Categorie\Categorie;
-//use charlymatloc\core\domain\entities\Reservation\Reservation;
-//use charlymatloc\core\domain\entities\User\User;
 use charlymatloc\infra\repositories\interface\OutilRepositoryInterface;
 use PDO;
 
@@ -14,8 +10,8 @@ class PDOOutilRepository implements OutilRepositoryInterface {
 
     private PDO $outil_pdo;
 
-    public function __construct(PDO $patient_pdo) {
-        $this->outil_pdo = $patient_pdo;
+    public function __construct(PDO $pdo) {
+        $this->outil_pdo = $pdo;
     }
 
     public function findOutilById(string $id): Outil{
@@ -36,85 +32,6 @@ class PDOOutilRepository implements OutilRepositoryInterface {
             $outil["modifie_quand"]
         );
     }
-
-//    public function findPanierById(string $id): Panier{
-//        $stmt = $this->pdo->prepare("SELECT *
-//        FROM panier
-//        WHERE id = :id");
-//        $stmt->execute(['id' => $id]);
-//        $outil = $stmt->fetch(PDO::FETCH_ASSOC);
-//        return new Outil(
-//            $outil["id"],
-//            $outil["idUser"],
-//            $outil["cree_par"],
-//            $outil["cree_quand"],
-//            $outil["modifie_par"],
-//            $outil["modifie_quand"]
-//        );
-//    }
-
-//    public function findCategorieById(string $id): Categorie{
-//        $stmt = $this->pdo->prepare("SELECT *
-//        FROM categorie
-//        WHERE id = :id");
-//        $stmt->execute(['id' => $id]);
-//        $outil = $stmt->fetch(PDO::FETCH_ASSOC);
-//        return new Outil(
-//            $outil["id"],
-//            $outil["nom"],
-//            $outil["description"],
-//            $outil["image"],
-//            $outil["tarifjournalier"],
-//            $outil["quantitestock"],
-//            $outil["idcat"],
-//            $outil["cree_par"],
-//            $outil["cree_quand"],
-//            $outil["modifie_par"],
-//            $outil["modifie_quand"]
-//        );
-//    }
-
-//    public function findReservationById(string $id): Reservation{
-//        $stmt = $this->pdo->prepare("SELECT *
-//        FROM reservation
-//        WHERE id = :id");
-//        $stmt->execute(['id' => $id]);
-//        $outil = $stmt->fetch(PDO::FETCH_ASSOC);
-//        return new Outil(
-//            $outil["id"],
-//            $outil["nom"],
-//            $outil["description"],
-//            $outil["image"],
-//            $outil["tarifjournalier"],
-//            $outil["quantitestock"],
-//            $outil["idcat"],
-//            $outil["cree_par"],
-//            $outil["cree_quand"],
-//            $outil["modifie_par"],
-//            $outil["modifie_quand"]
-//        );
-//    }
-//
-//    public function findUserById(string $id): User{
-//        $stmt = $this->pdo->prepare("SELECT *
-//        FROM users
-//        WHERE id = :id");
-//        $stmt->execute(['id' => $id]);
-//        $outil = $stmt->fetch(PDO::FETCH_ASSOC);
-//        return new Outil(
-//            $outil["id"],
-//            $outil["nom"],
-//            $outil["description"],
-//            $outil["image"],
-//            $outil["tarifjournalier"],
-//            $outil["quantitestock"],
-//            $outil["idcat"],
-//            $outil["cree_par"],
-//            $outil["cree_quand"],
-//            $outil["modifie_par"],
-//            $outil["modifie_quand"]
-//        );
-//    }
 
     public function findAllOutils(): array
     {
