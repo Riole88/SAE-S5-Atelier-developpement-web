c828dc770dbd2534d036d630a431940d0bc4cba9
Riole88 <boullemanitou@gmail.com>
2025-10-21 09:36:59 +0200

    ajout Panier.php

--- app/src/application_core/domain/entities/Utilisateur/Panier.php
+++ app/src/application_core/domain/entities/Utilisateur/Panier.php
@@ -2,7 +2,22 @@
 
 namespace charlymatloc\core\domain\entities\Utilisateur;
 
+use Faker\Core\Uuid;
+
 class Panier
 {
-
+    public function __construct(
+        private Uuid $id,
+        private Uuid $idUser,
+        private ?Uuid $creePar,
+        private \DateTimeInterface $creeQuand,
+        private ?Uuid $modifiePar,
+        private \DateTimeInterface $modifieQuand
+    ){}
+    public function __get(string $name){
+        if(property_exists($this,$name)) {
+            return $this->$name;
+        }
+        throw new \Exception("Propriété '$name' inexistante dans " . __CLASS__);
+    }
 }
