e4d3ca59bffb026fe9255e567b9ab342946d401a
tuline-leveque <tuline.leveque@gmail.com>
2025-10-22 11:22:03 +0200

    update(route): reserver
    Modification de la route d'exemple pour l'ajout d'un outil dans le panier.

--- app/src/api/routes.php
+++ app/src/api/routes.php
@@ -21,7 +21,7 @@ return function(App $app): App {
     //     POST
     $app->post('/outils/{id_outil}/reserver', ReserverOutilAction::class)
         ->add(new AjouterPanierValidationMiddleware());
-    //localhost:6080/outils/d8cf91f1-45f1-4d62-96a0-fda5c1e07f19/reserver?id_user=e3a5cf93-7b9c-40ac-a0c5-2b8b2b5e93b1&quantite=2&date_reservation=2025-12-04
+    //localhost:6080/outils/d8cf91f1-45f1-4d62-96a0-fda5c1e07f19/reserver?id_user=e3a5cf93-7b9c-40ac-a0c5-2b8b2b5e93b1&quantite=2&date_reservation=2025-12-04 20:00:00
 
     return $app;
 };
